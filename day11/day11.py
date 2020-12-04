a4775a06b191c9d8bf365e57f8c88761dedc6bc2
codingSince9 <franvlahovicjv@gmail.com>
2020-12-04 03:44:03 +0100

    Added day 11

---
+++ day11/day11.py
@@ -0,0 +1,81 @@
+password = open("day11.txt", "r").readline().strip()
+
+
+def increment(pwd):
+    counter = 0
+    pwd = ''.join(reversed(pwd))
+    pwdList = list(pwd)
+    for char in pwd:
+        if chr(ord(char) + 1).isalpha():
+            pwdList[counter] = chr(ord(char) + 1)
+            break
+        else:
+            pwdList[counter] = 'a'
+        counter += 1
+
+    pwd = ''.join(pwdList)
+    pwd = ''.join(reversed(pwd))
+
+    return pwd
+
+
+letterPairs = []
+secondPart = False
+while True:
+    twoSameChars = ""
+    doubleLetter = 0
+    increasingThree = ""
+    three = False
+    forbiddenLetter = False
+    letterPairs.clear()
+
+    for char in password:
+        if len(increasingThree) == 0:
+            increasingThree += char
+        elif len(increasingThree) == 1 and ord(increasingThree) + 1 == ord(char):
+            increasingThree += char
+        elif len(increasingThree) == 2 and ord(increasingThree[1]) + 1 == ord(char):
+            increasingThree += char
+            three = True
+        elif increasingThree != char:
+            increasingThree = ""
+
+        if char == 'i' or char == 'o' or char == 'l':
+            forbiddenLetter = True
+
+        if len(twoSameChars) == 0:
+            twoSameChars = char
+            continue
+        elif len(twoSameChars) == 1:
+            if char == twoSameChars:
+                twoSameChars += char
+                if twoSameChars not in letterPairs:
+                    doubleLetter += 1
+                    letterPairs.append(twoSameChars)
+            twoSameChars = char
+
+    if three and not forbiddenLetter and doubleLetter >= 2 and not secondPart:
+        print("Part 1:", password, 'AEEEEEEEEEEEEEEEEE')
+        passwordList = list(password)
+        password = increment(password)
+        secondPart = True
+        continue
+    if secondPart and three and not forbiddenLetter and doubleLetter >= 2:
+        print("Part 2:", password, 'AEEEEEEEEEEEEEEEEE')
+        break
+
+    counter = 0
+    passwordList = list(password)
+
+    # if i, o, l, then change letters after them to 'a'
+    for char in password:
+        if char == 'i' or char == 'o' or char == 'l':
+            passwordList[counter] = chr(ord(char) + 1)
+            for i in range(counter + 1, len(passwordList)):
+                passwordList[i] = 'a'
+            break
+        counter += 1
+
+    password = ''.join(passwordList)
+
+    password = increment(password)
